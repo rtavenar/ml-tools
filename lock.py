db582057e0268d68bd254deb08ee7c87ffad5357
Paul Viallard <paul.viallard@gmail.com>
2025-07-24 10:59:39 +0200

    fix: typo on the name!

--- lock.py
+++ lock.py
@@ -1,4 +1,4 @@
-# Copyright © 2025 Paul Viallard <paul.viallard@gmail.com> and Hind Atbir <hatbir.p@gmail.com>
+# Copyright © 2025 Paul Viallard <paul.viallard@gmail.com> and Hind Atbir <atbir.p@gmail.com>
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
